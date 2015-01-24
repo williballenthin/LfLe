a8535a9af396a92e629c3f77d25204ff8f6b6941
Willi Ballenthin <willi.ballenthin@gmail.com>
2015-01-24 14:12:45 -0500

    lfle: use python-evt

--- lfle.py
+++ lfle.py
@@ -7,58 +7,28 @@
 #
 # Dependencies:
 #   argparse (easy_install/pip)
+#   python-evt (easy_install/pip)
 
 import sys
-import struct
+import logging
 
 import argparse
 
-isVerbose = False
-isStatus = True
+try:
+    from Evt.Evt import EvtCarver
+except ImportError:
+    print("Error: you must have the python-evt package installed (use `pip`)!"
 
 
-def debug(s):
-    global verbose
-    if isVerbose:
-        print "# [d] %s" % (s)
+g_logger = logging.getLogger("lfle")
+BUFSIZE = 4096 * 1000
+MAXRECORD = 4096 * 16
+MINRECORD = 0x30
 
 
-def print_status(s):
-    global status
-    if isStatus:
-        sys.stdout.write(s)
-
-
-class LfleException(Exception):
-    def __init__(self, value):
-        self.value = value
-
-    def __str__(self):
-            return "LfleException: %s" % (self.value)
-
-
-class InvalidContents(LfleException):
-    def __init__(self, value):
-        super(InvalidContents, self).__init__(value)
-
-    def __str__(self):
-        return "InvalidContents: %s" % (self.value)
-
-
-class InvalidStructure(LfleException):
-    def __init__(self, value):
-        super(InvalidStructure, self).__init__(value)
-
-    def __str__(self):
-        return "InvalidStructure: %s" % (self.value)
-
-
-def doit(filename, outputfilename):
-    BUFSIZE = 4096 * 1000
-    MAXRECORD = 4096 * 16
-    MINRECORD = 0x30
-    o = open(outputfilename, "wb")
-
+def write_evt_header(filelikeobject):
+    # Evt header:
+    #
     # 0000   30 00 00 00 4C 66 4C 65 01 00 00 00 01 00 00 00    0...LfLe........
     # 0010   30 00 00 00 30 00 00 00 01 00 00 00 00 00 00 00    0...0...........
     # 0020   00 00 01 00 00 00 00 00 80 51 01 00 30 00 00 00    .........Q..0...
@@ -75,8 +45,13 @@ def doit(filename, outputfilename):
     # 0x24 (dword)flags	0x0
     # 0x28 (dword)retention	0x15180
     # 0x2c (dword)end_length	0x30
-    o.write("0\x00\x00\x00LfLe\x01\x00\x00\x00\x01\x00\x00\x000\x00\x00\x000\x00\x00\x00\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x01\x00\x00\x00\x00\x00\x80Q\x01\x000\x00\x00\x00")
+    filelikeobject.write("0\x00\x00\x00LfLe\x01\x00\x00\x00\x01\x00\x00\x000\x00\x00\x000\x00\x00\x00\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x01\x00\x00\x00\x00\x00\x80Q\x01\x000\x00\x00\x00")
+    filelikeobject.flush()
 
+
+def write_evt_cursor(filelikeobject):
+    # EVT cursor:
+    #
     # 0000   28 00 00 00 11 11 11 11 22 22 22 22 33 33 33 33    (.......""""3333
     # 0010   44 44 44 44 30 00 00 00 B8 5F 00 00 65 00 00 00    DDDD0...._..e...
     # 0020   01 00 00 00 28 00 00 00                            ....(...
@@ -88,159 +63,74 @@ def doit(filename, outputfilename):
     # 0x18 (dword)next_offset	0x58
     # 0x1c (dword)current_record_number	0x1
     # 0x20 (dword)oldest_record_number	0x1
-    o.write("(\x00\x00\x00\x11\x11\x11\x11\"\"\"\"3333DDDD0\x00\x00\x00\x58\x00\x00\x00\x01\x00\x00\x00\x01\x00\x00\x00(\x00\x00\x00")
+    filelikeobject.write("(\x00\x00\x00\x11\x11\x11\x11\"\"\"\"3333DDDD0\x00\x00\x00\x58\x00\x00\x00\x01\x00\x00\x00\x01\x00\x00\x00(\x00\x00\x00")
+    filelikeobject.flush()
+       
 
-    def write_buf(buf):
-        if buf.count("LfLe") > 1:
-            raise InvalidContents("More than 1 magic in copy!")
-        head = buf[:4]
-        tail = buf[-4:]
-        if head != tail:
-            raise InvalidStructure("Invalid structure")
-        o.write(buf)
+def do_carve(fin, fout):
+    write_evt_header(fout)
+    write_evt_cursor(fout)
 
-    def write_offset(offset, length):
-        with open(filename) as f:
-            f.seek(offset)
-            buf = f.read(length)
-            if buf.count("LfLe") > 1:
-                raise InvalidContents("More than 1 magic in write!")
-            head = buf[:4]
-            tail = buf[-4:]
-            if head != tail:
-                raise InvalidStructure("Invalid structure")
-            o.write(buf)
+    analyzer = EvtCarver(fin, chunksize=BUFSIZE, maxrecord=MAXRECORD)
 
-    def get_buf():
-        f = open(filename, "rb")
-        f.seek(0, 2)  # end
-        size = f.tell()
-        f.seek(0)     # begin
-        offset = 0
-        counter = 0
-        status = ""
-        while True:
-            f.seek(offset)
-            buf = f.read(2 * BUFSIZE)
-            if buf == "":
-                break
-            counter += 1
+    for entry in analyzer.carve():
+        fout.write(entry)
 
-            print_status("\b" * len(status))
-            status = "%s%% done" % (str(100 * float(offset) / size))
-            print_status(status)
-            sys.stdout.flush()
-
-            yield (buf, offset, counter)
-            offset += BUFSIZE
-        print_status("\b" * len(status))
-        status = "100% complete"
-        print_status(status)
-        sys.stdout.flush()
-
-        f.close()
-
-    skipped_large = 0
-    skipped_small = 0
-    skipped_structure = 0
-    skipped_contents = 0
-    records = []
-    for (buf, buf_offset, counter) in get_buf():
-        debug("new buffer @ %s" % (hex(buf_offset)))
-        if counter % 25600 == 0:
-            print_status(".")
-        offset = 4
-        new = True
-        while True:
-            debug("searching from %s to %s" % \
-                      (hex(offset), hex(BUFSIZE + 4)))
-            index = buf.find("LfLe", offset, BUFSIZE + 4)
-            if index == -1:
-                break
-            if index != offset and not new:
-                debug("slack space @ %s with length %s" % \
-                          (hex(offset), hex(index - offset)))
-            new = False
-            length = struct.unpack_from("<I", buf, index - 4)[0]
-            if length <= MINRECORD:  # evt file header
-                skipped_small += 1
-                offset = index + 1
-                continue
-            if length > MAXRECORD:
-                debug("length too long %s > %s" % \
-                        (hex(length), hex(MAXRECORD)))
-                skipped_large += 1
-                offset = index + 1
-                continue
-            records.append((buf_offset + index, length))
-            if offset + length < BUFSIZE:
-                debug("copying from %s (%s) with length %s" % \
-                        (hex(index - 4), hex(buf_offset + index - 4), length))
-                try:
-                    write_buf(buf[index - 4:index - 4 + length])
-                except InvalidContents:
-                    skipped_contents += 1
-                    debug("More than 1 magic in copy!")
-                except InvalidStructure:
-                    skipped_structure += 1
-                    debug("Invalid structure")
-            else:
-                debug("writing %s with length %s" % \
-                          (hex(buf_offset + index - 4), hex(length)))
-                try:
-                    write_offset(buf_offset + index - 4, length)
-                except InvalidContents:
-                    skipped_contents += 1
-                    debug("More than 1 magic in copy!")
-                except InvalidStructure:
-                    skipped_structure += 1
-                    debug("Invalid structure")
-            offset = index + length
-            if offset >= BUFSIZE + 4:
-                break
-
-    print_status("\n")
-    print "Wrote %d records" % len(records)
-    if skipped_large > 0:
-        print "Skipped %d records with length greater than %s" % \
-        (skipped_large, hex(MAXRECORD))
-    if skipped_small > 0:
-        print "Skipped %d records with length less than %s" % \
-        (skipped_small, hex(MINRECORD))
-    if skipped_structure > 0:
-        print "Skipped %d records with invalid structure" % \
-        (skipped_structure)
-    if skipped_contents > 0:
-        print "Skipped %d records with invalid content" % \
-        (skipped_contents)
-    o.close()
-
+    s = analyzer.get_status()
+    g_logger.info("Carved %d records", s.valid)
+    g_logger.info("Skipped %d records with length greater than %s",
+                  s.too_big, hex(MAXRECORD))
+    g_logger.info("Skipped %d records with length less than %s",
+                  s.too_small, hex(MINRECORD))
+    g_logger.info("Skipped %d records with invalid structure",
+                  s.bad_structure)
+    g_logger.info("Skipped %d records with invalid content",
+                  s.bad_content)
+            
 
 def main():
     parser = argparse.ArgumentParser(
         description='Recover event log entries from an image ' +
                     'by heurisitically looking for record structures.')
-    parser.add_argument('input_path', action="store",
+    parser.add_argument('input_path',
+                        nargs="?",
+                        default="STDIN",
                         help="Path to a raw (dd) image file.")
-    parser.add_argument('output_path', action="store",
+    parser.add_argument('output_path',
+                        nargs="?",
+                        default="STDOUT",
                         help="Path to write output file that contains " +
                         "recovered event log entries.")
-    parser.add_argument('-v', action="store_true", dest="verbose",
-                        help="Print debugging messages during scanning.")
-    parser.add_argument('-s', action="store_true", dest="nostatus",
-                        help="Disable status messages (percent complete) " +
-                              "during scanning.")
-    parser_results = parser.parse_args()
+    parser.add_argument("--verbose", action="store_true",
+                        help="Enable debugging logging")
+    args = parser.parse_args()
 
-    if parser_results.verbose:
-        global isVerbose
-        isVerbose = True
+    if args.verbose:
+        logging.basicConfig(level=logging.DEBUG)
+    else:
+        logging.basicConfig(level=logging.INFO)
 
-    if parser_results.nostatus:
-        global isStatus
-        isStatus = False
+    fin = None
+    if args.input_path == "STDIN":
+        import sys
+        fin = sys.stdin
+        g_logger.info("No input file provided, assuming STDIN")
+    else:
+        fin = open(args.input_path, "rb")
 
-    doit(parser_results.input_path, parser_results.output_path)
+    fout = None
+    if args.output_path == "STDOUT":
+        import sys
+        fout = sys.stdout
+        g_logger.info("No output file provided, assuming STDOUT")
+    else:
+        fout = open(args.input_path, "rb")
+
+    try:
+        do_carve(fin, fout)
+    finally:
+        fin.close()
+        fout.close()
 
 
 if __name__ == '__main__':
